6fb0ffe85c4d51c07249093099fc192a9af9ab54
7-18 <kevinbrianbr@gmail.com>
2023-05-26 16:26:37 -0500

    feat: add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { authGuard } from './guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    canActivate: [authGuard],
+    component: HomeComponent,
+  },
+  {
+    path: 'user-details/:id',
+    canActivate: [authGuard],
+    component: UserDetailsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
