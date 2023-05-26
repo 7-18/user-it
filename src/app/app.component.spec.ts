c02cb575e49a2e0a536894db4300cbab36299a50
7-18 <kevinbrianbr@gmail.com>
2023-05-26 16:27:11 -0500

    feat: add components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientModule],
+    declarations: [AppComponent, NavigationComponent, FooterComponent]
   }));
 
   it('should create the app', () => {
@@ -20,10 +23,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('user-it');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('user-it app is running!');
-  });
 });
